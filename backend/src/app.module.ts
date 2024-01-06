cf35ce7b88ecb58559c0e99a78acbbe6da9a6983
Shakil Hossain <hshakilst@gmail.com>
2024-01-06 18:55:40 +0100

    Setup Monorepo. Worked on backend project setup.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EnvHelper } from './common/helpers/env.helper';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: EnvHelper.determineEnvFilePath(),
+      validationSchema: EnvHelper.validationSchema(),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
