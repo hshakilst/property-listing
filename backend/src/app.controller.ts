a555b9aaa081e4e6a87b8ff7019f1b2081b6a5ee
Shakil Hossain <hshakilst@gmail.com>
2024-01-16 18:21:52 +0100

    Backend: Added Swagger Documentation

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -7,15 +7,66 @@ import {
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
+import {
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+  ApiConsumes,
+  ApiBody,
+  ApiQuery,
+  ApiParam,
+} from '@nestjs/swagger';
+
 import { AppService } from './app.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { FileTypeValidationPipe } from './common/pipes/file.pipe';
 import { EnumValidationPipe } from './common/pipes/enum.pipe';
 
+@ApiTags('Property')
 @Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  //! Upload Image Swagger Configuration
+  @ApiOperation({ summary: 'Uploads a Image to S3 bucket for a Property' })
+  @ApiConsumes('multipart/form-data')
+  @ApiParam({
+    name: 'externalId',
+    schema: {
+      example: '106705',
+    },
+  })
+  @ApiQuery({
+    name: 'source',
+    schema: {
+      example: 'hhs.texas.gov',
+    },
+  })
+  @ApiQuery({
+    name: 'type',
+    schema: {
+      example: 'Assisted Living Type B',
+    },
+  })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    schema: {
+      type: 'object',
+      example: {
+        url: 'https://property-listing.s3.us-west-1.amazonaws.com/images/hhs.texas.gov/Home+Health+Agency/016836/IMG_1134_sd.jpg',
+      },
+    },
+  })
   @Post('upload/:externalId')
   @UseInterceptors(
     FileInterceptor('file', {
@@ -24,6 +75,7 @@ export class AppController {
       },
     }),
   )
+  //! Upload Image Function
   async uploadImage(
     // Implement Joi Validation for request params and Also Response DTO.
     @Param('externalId') externalId: string,
@@ -40,12 +92,99 @@ export class AppController {
     return this.appService.uploadImage(externalId, type, source, file);
   }
 
+  //! Search Property Swagger Configuration
+  @ApiOperation({
+    summary:
+      'Search the Database by name/city/state. *Warning be careful using state as a query if the DB is populated (may hang the page).',
+  })
+  @ApiQuery({
+    name: 'q',
+    example: 'Austin',
+  })
+  @ApiResponse({
+    schema: {
+      example: [
+        {
+          externalId: '017190',
+          name: '1st Texas Home Health',
+          capacity: 0,
+          city: 'Sealy',
+          zip: '77474',
+          state: 'Texas',
+          imageUrls: [
+            'https://property-listing.s3.us-west-1.amazonaws.com/images/hhs.texas.gov/Home+Health+Agency/016836/IMG_1134_sd.jpg',
+          ],
+          address: '324 MEYER',
+          county: 'Austin',
+          phone: '979-877-0900',
+          type: 'Home Health Agency',
+          source: 'hhs.texas.gov',
+          mapUrl:
+            'https://www.google.com/maps/embed/v1/place?key=AIzaSyBZF8RwjhXobKAGSfq5ZEpVMdGkVRROGZA&q=324+MEYER,+Sealy,+TX+77474',
+          descriptions: ['Accepts Medicare: Yes'],
+          detailsUrl:
+            'https://apps.hhs.texas.gov/LTCSearch/providerdetail.cfm?pid=017190&protype=Home Health&subtype=&lang=EN',
+        },
+      ],
+    },
+  })
   @Get('search')
+  //! Search Property Function
   async searchProperty(@Query('q') searchTerm: string) {
     return this.appService.searchProperty(searchTerm);
   }
 
+  //! Get Property Swagger Configuration
+  @ApiOperation({
+    summary: 'Get one property from the DB.',
+  })
+  @ApiParam({
+    name: 'externalId',
+    schema: {
+      example: '106705',
+    },
+  })
+  @ApiQuery({
+    name: 'source',
+    schema: {
+      example: 'hhs.texas.gov',
+    },
+  })
+  @ApiQuery({
+    name: 'type',
+    schema: {
+      example: 'Assisted Living Type B',
+    },
+  })
+  @ApiResponse({
+    schema: {
+      example: {
+        externalId: '106705',
+        name: 'The Delaney at Georgetown Village',
+        capacity: 97,
+        city: 'Georgetown',
+        zip: '78633',
+        state: 'Texas',
+        imageUrls: [],
+        address: '359 VILLAGE COMMONS BOULEVARD',
+        county: 'Williamson',
+        phone: '512-819-9500',
+        type: 'Assisted Living Type B',
+        source: 'hhs.texas.gov',
+        mapUrl:
+          'https://www.google.com/maps/embed/v1/place?key=AIzaSyBZF8RwjhXobKAGSfq5ZEpVMdGkVRROGZA&q=359+VILLAGE+COMMONS+BOULEVARD,+Georgetown,+TX+78633',
+        descriptions: [
+          'Service Type: Type B',
+          'Total Bed Count: 97',
+          'Alzheimer Certification: Yes',
+        ],
+        detailsUrl:
+          'https://apps.hhs.texas.gov/LTCSearch/providerdetail.cfm?pid=106705&protype=Assisted Living&subtype=TYPE B&lang=EN',
+      },
+    },
+  })
   @Get('property/:externalId')
+  //! Get Property Function
   async getProperty(
     @Param('externalId') externalId: string,
     @Query('type') type: string,
@@ -54,7 +193,28 @@ export class AppController {
     return this.appService.getProperty(externalId, type, source);
   }
 
+  //! Control Crawler Swagger Configuration
+  @ApiOperation({
+    summary:
+      'Start or Stop the Crawler Service. Crawler Service Also Starts at Midnight 5 AM.',
+  })
+  @ApiParam({
+    name: 'command',
+    schema: {
+      type: 'string',
+      enum: ['start', 'stop'],
+      example: 'start',
+    },
+  })
+  @ApiResponse({
+    schema: {
+      example: {
+        message: 'Crawler started successfully.',
+      },
+    },
+  })
   @Get('crawler/:command')
+  //! Control Crawler Function
   async controlCrawler(
     @Param('command', new EnumValidationPipe(['start', 'stop']))
     command: 'start' | 'stop',
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  //Setup Swagger Documentation
+  const config = new DocumentBuilder()
+    .setTitle('Property Listing')
+    .setDescription(
+      'Assessment Task for BoomersHub. Currently only crawls hhs.texas.gov. Total Number of Records for hhs.texas.gov: 11961 (Last: 2024-01-11)',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/docs', app, document);
+
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT'); // Default to 8080 if PORT is not defined
+  const port = configService.get<number>('PORT');
 
   await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
